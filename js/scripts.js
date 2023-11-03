4bf33aa3b60a95d7b584d31d4d844170179088aa
Orel Butbul <orelbutbul13@gmail.com>
2023-11-03 12:31:38 -0700

    WIP updating Js file & branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,23 @@
 const form = document.querySelector("form");
-let userOutput = document.getElementById("final-result");
-
-let finalAnswer;
-
-
-
-
+let userOutput = " ";
 form.onsubmit = function(e){
-    e.preventDefault();
-    const userInput1 = document.getElementById("person1Input").value;
-    
-    if (userInput1 === "yes") {
-        finalAnswer = "Great, we can move on to the next question" ;
-    } else if ("no") {
-        finalAnswer = "I'm sorry, i have nothing to offer you";
-    } else {
-        finalAnswer = "you need to answer with yes or no only";
-    }
-
-    console.log(finalAnswer);
-    userOutput.innerText = finalAnswer;
+    e.preventDefault();;
 }
 
+const userInput1 = document.getElementById("person1Input").value;
+const userInput2 = document.getElementById("person2Input").value;
+const userInput3 = document.getElementById("person3Input").value;
+const userInput4 = document.getElementById("person4Input").value;
+const userInput5 = document.getElementById("person5Input").value;
 
-// const userInput2 = document.getElementById("person2Input").value;
-// const userInput3 = document.getElementById("person3Input").value;
-// const userInput4 = document.getElementById("person4Input").value;
-// const userInput5 = document.getElementById("person5Input").value;
-
+if (userInput1 === "") {
+    userOutput = "You are obliged to answer correctly and fill in the column";
+} else if (userInput1 === "yes") {
+        userOutput = "Stupendous! JaveScript is the programming language for you.";
+} else if (userInput2 === "yes") {
+    userOutput = "Atounding! C# is the programming language for you.";
+} else if (userInput3 === "yes") {
+    userOutput = "Fantastic! HTML is the programming language for you.";
+} else if (userInput4 === "yes") {
+    userOutput = "Luminescence! Ruby is the programming language for you";
+}
